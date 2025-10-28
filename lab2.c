beca1daea5a57434bd8c9efe5d9f0d03e64ed0f7
AlonEZ1 <Supakrit.non168@gmail.com>
2025-10-28 17:29:56 +0700

    first commit

---
+++ lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void){
+
+    int age = 17 ;
+    char name[50] = "Supakrit" ;
+    char gender = 'M' ;
+    float GPA = 4.00 ;
+
+    printf( "My Name is %s \n" , name) ; 
+    printf( "I'm %d \n" , age);
+    printf( "My Gender is %c \n" , gender ) ;
+    printf( "GPA is %f \n" , GPA) ;
+
+    return 0;
+
+}
